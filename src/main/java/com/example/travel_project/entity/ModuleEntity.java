3ead729c89d8d43f4d879c0d015413258f37b7c3
ywkangkai <709351930@qq.com>
2025-07-07 17:08:59 +0800

    添加表

--- src/main/java/com/example/travel_project/entity/ModuleEntity.java
+++ src/main/java/com/example/travel_project/entity/ModuleEntity.java
@@ -6,18 +6,13 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
-import java.util.List;
-
 @Data
-@TableName("tb_project")
-public class ProjectEntity extends BaseEntity{
-    @TableId(type= IdType.AUTO)
+@TableName("tb_module")
+public class ModuleEntity extends BaseEntity{
+    @TableId(type = IdType.AUTO)
     private Integer id;
     private String name;
-    private Integer LeaderId;
-    private String LeaderName;
     private String description;
-    private Boolean isDeleted;
-    @TableField(exist = false) // 非数据库字段
-    private List<ModuleEntity> modules; // 关联的模块实体
+    private Boolean isDelete;
+
 }
---
+++ src/main/java/com/example/travel_project/entity/TaskModuleEntity.java
@@ -0,0 +1,15 @@
+package com.example.travel_project.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+@Data
+@TableName("tb_task_module")
+public class TaskModuleEntity extends BaseEntity {
+    @TableId(type = IdType.AUTO)
+    private Integer id;
+    private Integer taskId;
+    private Integer moduleId;
+}
